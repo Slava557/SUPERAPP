90f26777e1bc49070d7f58ddc44c5041269bf617
Slava557 <arrormor@gmail.com>
2025-03-17 14:40:33 +0300

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc';
 import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  base: 'https://392bfdc9ea.tapps.global/latest', // Убедитесь, что здесь указан правильный base URL
+  base: 'https://slava557.github.io/SUPERAPP/', // Убедитесь, что здесь указан правильный base URL
   plugins: [
     react(),
     tsconfigPaths(),
